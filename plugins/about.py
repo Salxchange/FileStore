20a50e2a3504c91b62ec3f714c1b17a9d3f22bee
Salxchange <138751618+Salxchange@users.noreply.github.com>
2024-07-26 18:48:39 +0100

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -111,8 +111,8 @@ REPLY_ERROR = "<code>Use this command as a reply to any telegram message without
 async def not_joined(client: Client, message: Message):
     buttons = [
         [
-            InlineKeyboardButton(text="• ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=client.invitelink),
-            InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ •", url=client.invitelink2),
+            InlineKeyboardButton(text="Join Channel 1", url=client.invitelink),
+            InlineKeyboardButton(text="Join Channel 2", url=client.invitelink2),
         ]
     ]
     try:
